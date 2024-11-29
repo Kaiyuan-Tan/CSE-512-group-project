ab0023730d216ad3e63f41dcdf3e33579e91cf55
Kaiyuan-Tan <727353680@qq.com>
2024-11-28 19:19:24 -0700

    debug-datae

--- mongo.py
+++ mongo.py
@@ -348,8 +348,8 @@ if __name__ == "__main__":
         api_key=api_key
     )
 
-    # if client.indices.exists(index=INDEX_NAME):
-    #     client.indices.delete(index=INDEX_NAME)
+    if client.indices.exists(index=INDEX_NAME):
+        client.indices.delete(index=INDEX_NAME)
     if not client.indices.exists(index=INDEX_NAME):
         mappings = {
             "properties": {
